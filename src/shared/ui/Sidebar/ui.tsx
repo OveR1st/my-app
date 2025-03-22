2c66d50929418b3395842e701e162070722a5d53
Dmitriy <315overdose@gmail.com>
2025-03-22 10:32:30 +0200

    sidebar corrects

--- src/shared/ui/Sidebar/ui.tsx
+++ src/shared/ui/Sidebar/ui.tsx
@@ -1,5 +1,11 @@
 import styles from './index.module.scss';
 
 export const Sidebar = () => {
-  return <aside className={styles.sidebar}>Sidebar</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.container}>
+        <div>Sidebar content</div>
+      </div>
+    </aside>
+  );
 };
